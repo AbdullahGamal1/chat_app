c489d7d7697ce1795cf2a0bd25a391fe18842674
LENOVO <abdullah.gamal971@gmail.com>
2024-03-23 22:18:43 +0200

    Add to Firestore create message model reade from firestore and added that to screen

--- lib/constance/constance.dart
+++ lib/constance/constance.dart
@@ -2,4 +2,5 @@ import 'package:flutter/material.dart';
 
 const KPrimaryColor = Color(0xff2B475E);
 const KLogo = 'assets/images/scholar.png';
-const kMessageCollection = 'message';
+const KMessageCollection = 'messages';
+const KMessage = "message";
